efbdc33b867e85d7314280910fd41888c7b9a3e7
Shirish Koirala <shirishkoirala@gmail.com>
2023-11-08 11:02:19 +1100

    SearchScreen updates with viewbinding.

--- app/src/main/java/com/shirishkoirala/devchallange/SearchScreen.kt
+++ app/src/main/java/com/shirishkoirala/devchallange/SearchScreen.kt
@@ -1,7 +1,14 @@
 package com.shirishkoirala.devchallange
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.shirishkoirala.devchallange.databinding.SearchScreenBinding
 
-class SearchScreen: AppCompatActivity() {
-
+class SearchScreen : AppCompatActivity() {
+    private lateinit var binding: SearchScreenBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = SearchScreenBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+    }
 }
