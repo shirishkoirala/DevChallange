e6ee0dae784a9ecd95ce28d58fa854e591bae3f0
Shirish Koirala <shirishkoirala@gmail.com>
2023-11-09 17:44:34 +1100

    DetailScreen back button tweaked.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("com.google.dagger:hilt-android:2.46.1")
+    implementation ("com.google.android.material:material:1.10.0")
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.core:core-ktx:1.12.0")
